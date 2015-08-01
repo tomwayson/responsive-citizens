b18ec55436ed39853fd07dcaaa3d33db166352a4
yingxuegu <xueying.gu@maine.edu>
2015-08-01 13:10:35 -0700

    new update

--- src/app/config.js
+++ src/app/config.js
@@ -4,7 +4,7 @@
         citizenRequestLayerUrl: 'http://services5.arcgis.com/qfyMgDMPesqltCji/arcgis/rest/services/event_team/FeatureServer/0',
         // infoTemplate for reporting.
         infoTemplate: {
-                  title: '<b>Request ${objectid}</b>',
+                  title: '<b>${objectid}</b>',
                   content: '<span class="infoTemplateContentRowLabel">Publish Date: </span>' +
                       '<span class="infoTemplateContentRowItem">${requestdate:DateFormat}</span><br><span class="infoTemplateContentRowLabel">Address: </span>' +
                       '<span class="infoTemplateContentRowItem">${address}</span><br><span class="infoTemplateContentRowLabel">Name: </span>' +
@@ -12,7 +12,12 @@
                       '<span class="infoTemplateContentRowItem">${severity:severityDomainLookup}</span><br><span class="infoTemplateContentRowLabel">Description: </span>' +
                       '<span class="infoTemplateContentRowItem">${description}</span><br>'+
                       '<span class="infoTemplateContentRowLabel">Start Time: </span><span class="infoTemplateContentRowItem">${start_time}</span><br>'+
-                      '<span class="infoTemplateContentRowLabel">End Time: </span><span class="infoTemplateContentRowItem">${end_time}</span><br>'
+                      '<span class="infoTemplateContentRowLabel">End Time: </span><span class="infoTemplateContentRowItem">${end_time}</span><br>'+
+                      '<span class="infoTemplateContentRowLabel">Attend Number: </span><span class="infoTemplateContentRowItem">2</span><br>'+
+                      '<span class="infoTemplateContentRowLabel">Xueying Gu </span><span class="infoTemplateContentRowItem"><img src="http://graph.facebook.com/100001446274351/picture?type=small"></span><br>'+
+                      '<span class="infoTemplateContentRowLabel">Brianna Tu </span><span class="infoTemplateContentRowItem"><img src="http://graph.facebook.com/720796341/picture?type=small"></span><br>'+
+                      '<button type="button">Attend</button>'     
+
                   },
         //Severity Field Domain Value Dictionary        
         severityFieldDomainCodedValuesDict: 
